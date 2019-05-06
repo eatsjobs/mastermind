adec996e75aebad6748dbb88f5782e221ace861f
Pasquale Mangialavori <p.mangialavori@luisaviaroma.com>
2019-05-06 17:46:48 +0200

    +configure router to work also with gh-pages

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Game, Home, GameOver } from './screens';
 const Routes = (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Route exact path='/' component={Home} />
     <Route path='/play' component={Game} />
     <Route path='/gameover' component={GameOver} />
