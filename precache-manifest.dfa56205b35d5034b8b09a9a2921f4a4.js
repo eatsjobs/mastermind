89d3cfe4023cf4226ddee4b48d623f325b49f5b1
Pasquale Mangialavori <p.mangialavori@luisaviaroma.com>
2019-05-06 17:33:56 +0200

    Updates

---
+++ precache-manifest.dfa56205b35d5034b8b09a9a2921f4a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7145421b479e3b33c73eb68cb5952f34",
+    "url": "/mastermind/index.html"
+  },
+  {
+    "revision": "dfd6d67d2efbe2e1f671",
+    "url": "/mastermind/static/css/main.3838bbfe.chunk.css"
+  },
+  {
+    "revision": "b5432704f8debd90fb5e",
+    "url": "/mastermind/static/js/2.cb40b400.chunk.js"
+  },
+  {
+    "revision": "dfd6d67d2efbe2e1f671",
+    "url": "/mastermind/static/js/main.068127c1.chunk.js"
+  },
+  {
+    "revision": "5ef1bc21d77223317b3d",
+    "url": "/mastermind/static/js/runtime~main.004a5c91.js"
+  }
+]);
